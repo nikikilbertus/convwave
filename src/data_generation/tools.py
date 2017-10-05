6211038916fdeb506238a5d5cec38058d65cf9c0
Timothy Gebhard <gebhard.mgg@googlemail.com>
2017-10-05 21:01:16 +0200

    Minor changes to make PyLint happy.

--- src/data_generation/tools.py
+++ src/data_generation/tools.py
@@ -1,3 +1,7 @@
+"""
+Provides a number of functions for the sample generation process.
+"""
+
 # -----------------------------------------------------------------------------
 # IMPORTS
 # -----------------------------------------------------------------------------
@@ -9,8 +13,8 @@ import numpy as np
 import pandas as pd
 import h5py
 
-from scipy.signal import butter, filtfilt
 from matplotlib import mlab
+from scipy.signal import butter, filtfilt
 from scipy.interpolate import interp1d
 
 
@@ -31,8 +35,8 @@ def progress_bar(current_value, max_value, elapsed_time=0, bar_length=50):
 
     # Construct the actual progress bar
     percent = float(current_value) / max_value
-    bar = '=' * int(round(percent * bar_length))
-    spaces = '-' * (bar_length - len(bar))
+    full_bar = '=' * int(round(percent * bar_length))
+    empty_bar = '-' * (bar_length - len(full_bar))
 
     # Calculate the estimated time remaining
     eta = elapsed_time / percent - elapsed_time
@@ -40,8 +44,8 @@ def progress_bar(current_value, max_value, elapsed_time=0, bar_length=50):
     # Collect the outputs and write them to stdout; move the carriage back
     # to the start of the line so that the progress bar is always updated.
     out = ("\r[{0}] {1}% ({2}/{3}) | {4:.1f}s elapsed | ETA: {5:.1f}s".
-           format(bar + spaces, int(round(percent * 100)), current_value,
-                  max_value, elapsed_time, eta))
+           format(full_bar + empty_bar, int(round(percent * 100)),
+                  current_value, max_value, elapsed_time, eta))
     sys.stdout.write(out)
     sys.stdout.flush()
 
@@ -65,27 +69,35 @@ def apply_psd(signal_t, psd, sampling_rate=4096, apply_butter=False):
     """
 
     # First set some parameters for computing power spectra
-    n = len(signal_t)
-    dt = 1./sampling_rate
+    signal_size = len(signal_t)
+    delta_t = 1 / sampling_rate
 
     # Go into Fourier (frequency) space: signal_t -> signal_f
-    frequencies = np.fft.rfftfreq(n, dt)
+    frequencies = np.fft.rfftfreq(signal_size, delta_t)
     signal_f = np.fft.rfft(signal_t)
 
     # Divide by the given Power Spectral Density (PSD)
     # This is the 'whitening' = actually adding color
-    color_signal_f = signal_f / (np.sqrt(psd(frequencies) / dt / 2.))
+    color_signal_f = signal_f / (np.sqrt(psd(frequencies) / delta_t / 2))
 
     # Go back into time space: color_signal_f -> color_signal_t
-    color_signal_t = np.fft.irfft(color_signal_f, n=n)
+    color_signal_t = np.fft.irfft(color_signal_f, n=signal_size)
 
     # In case we want to use a Butterworth-filter, here's how to do it:
     if apply_butter:
+
+        # Define cut-off frequencies for the filter
         f_low = 1
         f_high = 600
-        bb, ab = butter(4, [f_low*2/4096, f_high*2/4096], btype="bandpass")
+
+        # Calculate Butterworth-filter and normalization
+        numerator, denominator = butter(4, [f_low*2/4096, f_high*2/4096],
+                                        btype="bandpass")
         normalization = np.sqrt((f_high - f_low) / (sampling_rate / 2))
-        color_signal_t = filtfilt(bb, ab, color_signal_t) / normalization
+
+        # Apply filter and normalize
+        color_signal_t = filtfilt(numerator, denominator, color_signal_t)
+        color_signal_t = color_signal_t / normalization
 
     return color_signal_t
 
@@ -109,12 +121,31 @@ def get_psd(real_strain, sampling_rate=4096):
     nfft = 2 * sampling_rate  # Bigger values yield better resolution?
 
     # Use matplotlib.mlab to calculate the PSD from the real strain
-    P_xx, freqs = mlab.psd(real_strain, NFFT=nfft, Fs=sampling_rate)
+    power_spectrum, frequencies = mlab.psd(real_strain,
+                                           NFFT=nfft,
+                                           Fs=sampling_rate)
 
     # Interpolate it linearly, so we can re-sample the spectrum arbitrarily
-    psd = interp1d(freqs, P_xx)
+    psd = interp1d(frequencies, power_spectrum)
 
     return psd
+
+
+# -----------------------------------------------------------------------------
+
+
+def chirp_mass(mass1, mass2):
+    """
+    Takes two masses and calculates the corresponding chirpmass.
+    Args:
+        mass1: Mass 1
+        mass2: Mass 2
+
+    Returns:
+        chirpmass: The chirpmass that corresponds to mass1, mass2
+    """
+
+    return (mass1 * mass2) ** (3 / 5) / (mass1 + mass2) ** (1 / 5)
 
 
 # -----------------------------------------------------------------------------
@@ -131,13 +162,9 @@ def get_waveforms_as_dataframe(waveforms_path):
         waveforms_path: The path to the HDF file containing the waveforms
 
     Returns:
-        df: A pandas DataFrame containing all valid waveforms and their
+        dataframe: A pandas DataFrame containing all valid waveforms and their
             corresponding masses, chirpmasses and distances.
     """
-
-    # Shorthand definition of the chirp mass
-    def chirp_mass(mass1, mass2):
-        return (mass1 * mass2) ** (3 / 5) / (mass1 + mass2) ** (1 / 5)
 
     # Read in the actual waveforms, the config string (and parse from JSON),
     # and the indices of the failed waveforms
@@ -149,19 +176,20 @@ def get_waveforms_as_dataframe(waveforms_path):
     # Create a Pandas DataFrame containing only the relevant columns from the
     # config string (other columns are all trivial at this point)
     columns = ['distance', 'mass1', 'mass2']
-    df = pd.DataFrame(config, columns=columns)
+    dataframe = pd.DataFrame(config, columns=columns)
 
     # Add columns for the actual waveforms and the chirp masses
-    df['waveform'] = list(waveforms)
-    df['chirpmass'] = df.apply(lambda row: chirp_mass(row.mass1, row.mass2),
-                               axis=1)
+    dataframe['waveform'] = list(waveforms)
+    dataframe['chirpmass'] = dataframe.apply(lambda row: chirp_mass(row.mass1,
+                                                                    row.mass2),
+                                             axis=1)
 
     # Drop the rows with the failed waveforms, and reset the index
     # noinspection PyUnresolvedReferences
-    df = df.drop(list(failed_idx)).reset_index(drop=True)
+    dataframe = dataframe.drop(list(failed_idx)).reset_index(drop=True)
 
     # Resort columns to order them alphabetically
-    df = df[sorted(df.columns)]
+    dataframe = dataframe[sorted(dataframe.columns)]
 
     # Return the final DataFrame
-    return df
+    return dataframe
